f6b447e0fb9aefc6d5de89884a40382aa903f977
ashleerollice@MB-P <ashleerollice@students.abtech.edu>
2025-03-06 19:29:52 -0500

    Mobile menu added

--- private/shared/header.php
+++ private/shared/header.php
@@ -37,13 +37,16 @@ if (!defined('PUBLIC_PATH')) {
 
     <p><em>Your central resource for farmers markets in Western North Carolina</em></p>
 
-    <nav>
+    <input type="checkbox" id="menu-toggle-checkbox" hidden>
+    <!-- Label acts as the hamburger icon -->
+    <label for="menu-toggle-checkbox" id="menu-toggle-label" aria-label="Toggle navigation">☰</label>
+    <!-- Navigation Menu -->
+    <nav id="nav-menu">
       <ul>
-        <li><a href="<?= PUBLIC_PATH ?>/index.php">Home</a></li>
-        <li><a href="<?= PUBLIC_PATH ?>/regions.php">Regions</a></li>
-        <li><a href="<?= PUBLIC_PATH ?>/markets.php">Markets</a></li>
-        <li><a href="<?= PUBLIC_PATH ?>/vendors.php">Vendors</a></li>
-        <li><a href="<?= PUBLIC_PATH ?>/about.php">About</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="vendors.php">Vendors</a></li>
+        <li><a href="contact.php">Contact</a></li>
       </ul>
     </nav>
   </header>
